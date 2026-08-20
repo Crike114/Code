508d81f5c1b8aec4ee22c719ca87d44c8a8a216b
CrikeNotFound <156631284+Crike114@users.noreply.github.com>
2026-08-20 08:35:11 +0800

    2026.8.20 - 模板

--- 模板.cpp
+++ 模板.cpp
@@ -1,10 +1,23 @@
 #include<bits/stdc++.h>
-#define int long long;
+#include<bits/extc++.h>
+#define cxx __gnu_cxx
+#define pbds __gnu_pbds
+#define inf (INT_MAX >> 1)
+#define llinf (LLONG_MAX >> 1)
+#define for_0(rangeNum) for(int i = 0;i < rangeNum;i++)
+#define for_1(rangeNum) for(int i = 1;i <= rangeNum;i++)
+#define lc (mid << 1)
+#define rc (mid << 1 | 1)
+#define mid ((l + r) >> 1)
+#define lg(log2Num) __lg(log2Num)
+// #define int long long
+
 using namespace std;
 
 
 
 signed main(){
 
-    return 0;
+
+	return 0;
 }
